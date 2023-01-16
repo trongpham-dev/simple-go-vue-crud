cd0955fb1b794d65eac92d26525e11bd1a7df56a
trongpv <trongpv@unitech.vn>
2023-01-16 19:04:16 +0900

    init

---
+++ server/router/router.go
@@ -0,0 +1,29 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler) {
+	r = gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Typegin
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
